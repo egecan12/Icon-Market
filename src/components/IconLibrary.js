5229b9ab74798f419c19a1adcd4bd826517cce74
egecan12 <egecan18k@gmail.com>
2025-09-07 21:03:16 +0200

    Initialize React project with Vite, ESLint, and basic structure. Added .gitignore, package.json, and essential files for a React application. Included icon data and components for icon library functionality.

---
+++ src/components/IconLibrary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import iconsData from '../data/icon-index.json'
+
+
+export default function iconLibrary() {
+  return (
+    <div>iconLibrary
+        console.log(iconsData)
+    </div>
+  )
+}
